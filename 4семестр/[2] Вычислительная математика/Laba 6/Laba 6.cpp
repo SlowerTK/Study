2b401ca9739892d44debba70af230242ae60bb08
SlowerTK <ComicAntonius@gmail.com>
2024-05-14 15:44:18 +0700

    added some lab

---
+++ 4семестр/[2] Вычислительная математика/Laba 6/Laba 6.cpp
@@ -0,0 +1,268 @@
+﻿//Приблежение сплайнами
+//Для выполнения практической работы необходимо реализовать в программе интерполяцию табличной функции линейными, кубическими и параболическими сплайнами.
+//Формат входного файла: 
+//k – порядок сплайна(1 – линейный, 2 – параболический, 3 – кубический);
+//n – количество сплайнов;
+//x0…xn – узлы сетки;
+//y0…yn – значения функции в узлах сетки;
+//i Ai – граничные условия(для k = 2);
+//B0 Bn – граничные условия(для k = 3);
+//m – количество интервалов в результирующей сетке(т.е.количество узлов – m + 1, что сделано для унификации с узлами исходной сетки);
+//x0…xm – узлы результирующей сетки.
+// t – любой символ или строка, сообщающая, известно или нет аналитическое выражение для функции f(x);
+// f(x) – аналитическое выражение для функции (если оно известно).
+//Формат выходного файла:
+//a0 b0 c0 d0 – коэффициенты сплайнов (естественно, что коэффициенты c указываются только для k = 2 и k = 3, коэффициенты d – только для k = 3);
+//a1 b1 c1 d1
+//...
+//an - 1 bn - 1 cn - 1 dn - 1 
+//x0 S(x0) – значение сплайна в узлах результирующей сетки;
+//x1 S(x1)
+//...
+//xm S(xm)
+//eps - СКО (если аналитическое выражение для функции известно).
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include "PolStr.h"
+
+struct SplineData {
+	int splineOrder;
+	int numSplines;
+	std::vector<double> xValues;
+	std::vector<double> yValues;
+	std::vector<double> boundaryConditions;
+	int numIntervals;
+	std::vector<double> resultGridNodes;
+	std::string analyticalSimbol;
+	std::string function;
+};
+
+SplineData readInputFile(const std::string& filename) {
+	std::ifstream inputFile(filename);
+	SplineData data;
+
+	if(inputFile.is_open()) {
+		inputFile >> data.splineOrder >> data.numSplines;
+		data.xValues.resize(data.numSplines + 1);
+		data.yValues.resize(data.numSplines + 1);
+
+		for(int i = 0; i <= data.numSplines; ++i) {
+			inputFile >> data.xValues[i];
+		}
+
+		for(int i = 0; i <= data.numSplines; ++i) {
+			inputFile >> data.yValues[i];
+		}
+
+		if(data.splineOrder == 2 || data.splineOrder == 3) {
+			data.boundaryConditions.resize(2);
+			for(int i = 0; i < 2; ++i) {
+				inputFile >> data.boundaryConditions[i];
+			}
+		}
+
+		inputFile >> data.numIntervals;
+		data.resultGridNodes.resize(data.numIntervals + 1);
+		for(int i = 0; i <= data.numIntervals; ++i) {
+			inputFile >> data.resultGridNodes[i];
+		}
+
+		inputFile >> data.analyticalSimbol;
+		if(data.analyticalSimbol != "n") {
+			inputFile >> data.function;
+		}
+
+		inputFile.close();
+	}
+	else {
+		std::cerr << "Unable to open file " << filename << std::endl;
+	}
+
+	return data;
+}
+
+void linearSplineInterpolation(const SplineData& data, const std::string& outputFile) {
+	std::ofstream outFile(outputFile);
+	if(!outFile.is_open()) {
+		std::cout << "Unable to open file.";
+		return;
+	}
+
+	//выполнение интерполяции линейными сплайнами
+	std::vector<double> a(data.numSplines), b(data.numSplines);
+	for(int i = 0; i < data.numSplines; ++i) {
+		a[i] = data.yValues[i];
+		b[i] = (data.yValues[i + 1] - data.yValues[i]) / (data.xValues[i + 1] - data.xValues[i]);
+	}
+
+	//запись коэффициентов в выходной файл
+	outFile << "a b" << std::endl;
+	for(int i = 0; i < data.numSplines; ++i) {
+		outFile << a[i] << " " << b[i] << std::endl;
+	}
+
+	//запись значений сплайна в узлах результирующей сетки
+	outFile << "x S(x)" << std::endl;
+	for(int i = 0; i < data.numIntervals; ++i) {
+		outFile << data.resultGridNodes[i] << " " << (a[i] + b[i] * data.resultGridNodes[i]) << std::endl;
+	}
+
+	//если аналитическое выражение для функции известно, то посчитать СКО
+	if(data.analyticalSimbol != "n") {
+		char* polStr = new char[data.function.size() + 1];
+		strcpy_s(polStr, data.function.size() + 1, data.function.c_str());
+		char* Str = CreatePolStr(polStr, 0);
+		double sum = 0.0;
+		for(int i = 0; i < data.numIntervals; ++i) {
+			double Sx = a[i] + b[i] * data.resultGridNodes[i];
+			sum += (Sx - EvalPolStr(Str, data.resultGridNodes[i])) * (Sx - EvalPolStr(Str, data.resultGridNodes[i]));
+		}
+		outFile << "eps = " << sqrt(sum / data.numIntervals) << std::endl;
+	}
+
+	outFile.close();
+}
+
+void parabolaSplainInterpolation(const SplineData& data, const std::string& outputFilename) {
+	std::ofstream outputFile(outputFilename);
+
+	if(!outputFile.is_open()) {
+		std::cerr << "Unable to open file " << outputFilename << std::endl;
+		return;
+	}
+
+	//выполнение интерполяции параболическими сплайнами
+	std::vector<double> a(data.numSplines), b(data.numSplines), c(data.numSplines);
+	for(int i = 0; i < data.numSplines - 1; ++i) {
+		a[i] = data.yValues[i];
+		b[i] = (data.yValues[i + 1] - data.yValues[i]) / (data.xValues[i + 1] - data.xValues[i]);
+		c[i] = (b[i + 1] - b[i]) / (data.xValues[i + 2] - data.xValues[i]);
+	}
+
+	//запись коэффициентов в выходной файл
+	outputFile << "a b c" << std::endl;
+	for(int i = 0; i < data.numSplines; ++i) {
+		outputFile << a[i] << " " << b[i] << " " << c[i] << std::endl;
+	}
+
+	//запись значений сплайна в узлах результирующей сетки
+	outputFile << "x S(x)" << std::endl;
+	for(int i = 0; i < data.numIntervals; ++i) {
+		outputFile << data.resultGridNodes[i] << " " << (a[i] + b[i] * data.resultGridNodes[i] + c[i] * data.resultGridNodes[i] * data.resultGridNodes[i]) << std::endl;
+	}
+	//если аналитическое выражение для функции известно, то посчитать СКО
+	if(data.analyticalSimbol != "n") {
+		char* polStr = new char[data.function.size() + 1];
+		strcpy_s(polStr, data.function.size() + 1, data.function.c_str());
+		char* Str = CreatePolStr(polStr, 0);
+		double sum = 0.0;
+		for(int i = 0; i < data.numIntervals; ++i) {
+			double Sx = a[i] + b[i] * data.resultGridNodes[i] + c[i] * data.resultGridNodes[i] * data.resultGridNodes[i];
+			sum += (Sx - EvalPolStr(Str, data.resultGridNodes[i])) * (Sx - EvalPolStr(Str, data.resultGridNodes[i]));
+		}
+		outputFile << "eps = " << sqrt(sum / data.numIntervals) << std::endl;
+	}
+
+	outputFile.close();
+}
+
+void cubicSplineInterpolation(const SplineData& data, const std::string& outputFilename) {
+	std::ofstream outputFile(outputFilename);
+
+	if(!outputFile.is_open()) {
+		std::cerr << "Unable to open file" << outputFilename << std::endl;
+		return;
+	}
+
+	// Выполнение интерполяции кубическими сплайнами
+	std::vector<double> h(data.numIntervals);
+	std::vector<double> alpha(data.numIntervals);
+	std::vector<double> l(data.numIntervals + 1, 1.0);
+	std::vector<double> mu(data.numIntervals + 1, 0.0);
+	std::vector<double> z(data.numIntervals + 1, 0.0);
+	std::vector<double> a(data.numIntervals), b(data.numIntervals), c(data.numIntervals), d(data.numIntervals);
+
+	// Вычисление разностей между узлами
+	for(int i = 0; i < data.numIntervals; ++i) {
+		h[i] = data.xValues[i + 1] - data.xValues[i];
+	}
+
+	// Вычисление коэффициентов для метода прогонки
+	for(int i = 1; i < data.numIntervals; ++i) {
+		alpha[i] = (3.0 / h[i]) * (data.yValues[i + 1] - data.yValues[i]) - (3.0 / h[i - 1]) * (data.yValues[i] - data.yValues[i - 1]);
+	}
+
+	// Решение системы линейных уравнений для нахождения вторых производных
+	for(int i = 1; i < data.numIntervals; ++i) {
+		l[i] = 2.0 * (data.xValues[i + 1] - data.xValues[i - 1]) - h[i - 1] * mu[i - 1];
+		mu[i] = h[i] / l[i];
+		z[i] = (alpha[i] - h[i - 1] * z[i - 1]) / l[i];
+	}
+
+	// Обратный ход метода прогонки
+	for(int j = data.numIntervals-2; j > 0; --j) {
+		c[j] = z[j] - mu[j] * c[j + 1];
+		b[j] = (data.yValues[j + 1] - data.yValues[j]) / h[j] - h[j] * (c[j + 1] + 2.0 * c[j]) / 3.0;
+		d[j] = (c[j + 1] - c[j]) / (3.0 * h[j]);
+		a[j] = data.yValues[j];
+	}
+
+
+	// Запись коэффициентов в выходной файл
+	outputFile << "a b c d" << std::endl;
+	for(int i = 0; i < data.numIntervals; ++i) {
+		outputFile << a[i] << " " << b[i] << " " << c[i] << " " << d[i] << std::endl;
+	}
+
+	// Запись значений сплайна в узлах результирующей сетки
+	outputFile << "x S(x)" << std::endl;
+	for(int i = 0; i < data.numIntervals; ++i) {
+		double t = 0.1;
+		double x = data.xValues[i] + t * h[i];
+		double Sx = a[i] + b[i] * t + c[i] * t * t + d[i] * t * t * t;
+		outputFile << x << " " << Sx << std::endl;
+	}
+	//если аналитическое выражение для функции известно, то посчитать СКО
+	if(data.analyticalSimbol != "n") {
+		char* polStr = new char[data.function.size() + 1];
+		strcpy_s(polStr, data.function.size() + 1, data.function.c_str());
+		char* Str = CreatePolStr(polStr, 0);
+		double sum = 0.0;
+		for(int i = 0; i < data.numIntervals; ++i) {
+			double t = 0.1;
+			double x = data.xValues[i] + t * h[i];
+			double Sx = a[i] + b[i] * t + c[i] * t * t + d[i] * t * t * t;
+			sum += (Sx - EvalPolStr(Str, x)) * (Sx - EvalPolStr(Str, x));
+		}
+		outputFile << "eps = " << sqrt(sum / data.numIntervals) << std::endl;
+	}
+
+	outputFile.close();
+}
+
+
+int main() {
+	std::string filename = "input.txt";
+	SplineData data = readInputFile(filename);
+	std::string outputFile = "output.txt";
+
+	switch(data.splineOrder) {
+	case 1:
+		linearSplineInterpolation(data, outputFile);
+		break;
+	case 2:
+		parabolaSplainInterpolation(data, outputFile);
+		break;
+	case 3:
+		cubicSplineInterpolation(data, outputFile);
+		break;
+	default:
+		break;
+	}
+
+
+	return 0;
+}
---
+++ 4семестр/[2] Вычислительная математика/Laba 7/Laba 7.cpp
@@ -0,0 +1,296 @@
+﻿//ЧИСЛЕННОЕ ИНТЕГРИРОВАНИЕ ФУНКЦИЙ
+//реализовать четыре обязательных метода численного интегрирования функций – левосторонних и правосторонних прямоугольников, трапеций и Симпсона и, по желанию, один из двух дополнительных – Чебышева или Гаусса.
+//Формат входного файла: 
+//m – формула интегрирования(в порядке их перечисления), при m = 5 используется дополнительный метод;
+//g  – любой символ или строка, задающие тип сетки: равномерная, неравномерная, динамическая(при m != 5);
+//n  – количество интервалов интегрирования(если используется формула Симпсона, то кратно двум);
+//a b – границы отрезка(если сетка не является неравномерной или m = 5);
+//x0…xn – узлы сетки(если она неравномерная);
+//s – любой символ или строка, определяющие способ задания функции, если сетка не динамическая и m != 5 (табличная, аналитическая);
+//y0…yn – значения функции в узлах сетки(если она задана таблично);
+//f(x) – аналитическое выражение для функции(если сетка динамическая или m = 5);
+//ε – точность вычисления интеграла на динамической сетке.
+//
+//Формат выходного файла: 
+//I – значение интеграла;
+//k – количество итераций(для динамической сетки);
+//ε*  – достигнутая точность(для динамической сетки);
+//ti – абсциссы точек интегрирования(при m = 5);
+//Ai – коэффициенты Ai для формулы Гаусса.
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include "PolStr.h"
+
+
+void out2File(double result, int k, double eps) {
+	std::ofstream out("output.txt");
+	if(!out.is_open()) {
+		return;
+	}
+	out << "I: " << result;
+	if(k != 0) {
+		out << std::endl << "k: " << k << std::endl << "eps*: " << eps;
+	}
+	out.close();
+}
+void LeftRectangleMethod(double& a, double& b, int& n, std::string& g, std::vector<double>& y, std::vector<double>& x, char*& str, double eps) {
+	double sum = 0;
+	int k = 0;
+	double eps1 = 0;
+	if(g == "r") {
+		for(int i = 0; i < n; i++) {
+			sum += y[i];
+		}
+		sum *= (b - a) / 2;
+	}
+	else if(g == "n") {
+		for(int i = 0; i < n; i++) {
+			sum += y[i] * (x[static_cast<std::vector<double, std::allocator<double>>::size_type>(i) + 1] - x[i]);
+		}
+	}
+	else if(g == "d") {
+		do {
+			k++;
+			double h = (b - a) / n;
+			double sum1 = 0;
+			for(int i = 0; i < n; i++) {
+				sum1 += EvalPolStr(str, a + i * h) * h;
+			}
+			if(abs(sum1 - sum) < eps) {
+				eps1 = abs(sum1 - sum);
+				break;
+			}
+			sum = sum1;
+			n *= 2;
+		} while(true);
+	}
+	out2File(sum, k, eps1);
+}
+void RightRectangleMethod(double& a, double& b, int& n, std::string& g, std::vector<double>& y, std::vector<double>& x, char*& str, double eps) {
+	double sum = 0;
+	int k = 0;
+	double eps1 = 0;
+	if(g == "r") {
+		for(int i = 1; i <= n; i++) {
+			sum += y[i];
+		}
+		sum *= (b - a) / 2;
+	}
+	else if(g == "n") {
+		for(int i = 0; i < n; i++) {
+			sum += y[static_cast<std::vector<double, std::allocator<double>>::size_type>(i) + 1] * (x[static_cast<std::vector<double, std::allocator<double>>::size_type>(i) + 1] - x[i]);
+		}
+	}
+	else if(g == "d") {
+		do {
+			k++;
+			double h = (b - a) / n;
+			double sum1 = 0;
+			for(int i = 0; i < n; i++) {
+				sum1 += EvalPolStr(str, a + (i + 1) * h) * h;
+			}
+			if(abs(sum1 - sum) < eps) {
+				eps1 = abs(sum1 - sum);
+				break;
+			}
+			sum = sum1;
+			n *= 2;
+		} while(true);
+	}
+	out2File(sum, k, eps1);
+}
+void TrapezoidMethod(double& a, double& b, int& n, std::string& g, std::vector<double>& y, std::vector<double>& x, char*& str, double eps) {
+	double sum = 0;
+	int k = 0;
+	double eps1 = 0;
+	if(g == "r") {
+		for(int i = 1; i < n; i++) {
+			sum += y[i];
+		}
+		sum = (sum + (y[0] + y[n]) / 2) * (b - a) / n;
+	}
+	else if(g == "n") {
+		for(int i = 0; i < n; i++) {
+			sum += (y[i] + y[static_cast<std::vector<double, std::allocator<double>>::size_type>(i) + 1]) * (x[static_cast<std::vector<double, std::allocator<double>>::size_type>(i) + 1] - x[i]) / 2;
+		}
+	}
+	else if(g == "d") {
+		do {
+			k++;
+			double h = (b - a) / n;
+			double sum1 = 0;
+			for(int i = 0; i < n; i++) {
+				sum1 += (EvalPolStr(str, a + i * h) + EvalPolStr(str, a + (i + 1) * h)) * h / 2;
+			}
+			if(abs(sum1 - sum) < eps) {
+				eps1 = abs(sum1 - sum);
+				break;
+			}
+			sum = sum1;
+			n *= 2;
+		} while(true);
+	}
+	out2File(sum, k, eps1);
+}
+void SimpsonMethod(double& a, double& b, int& n, std::string& g, std::vector<double>& y, std::vector<double>& x, char*& str, double eps) {
+	double sum = 0;
+	int k = 0;
+	double eps1 = 0;
+	if(g == "r") {
+		for(int i = 1; i < n; i++) {
+			if(i % 2 == 0) {
+				sum += 2 * y[i];
+			}
+			else {
+				sum += 4 * y[i];
+			}
+		}
+		sum = (sum + y[0] + y[n]) * (b - a) / (3 * n);
+	}
+	else if(g == "n") {
+		for(int i = 0; i < n; i++) {
+			if(i % 2 == 0) {
+				sum += 2 * (y[i] + y[static_cast<std::vector<double, std::allocator<double>>::size_type>(i) + 1]);
+			}
+			else {
+				sum += 4 * (y[i] + y[static_cast<std::vector<double, std::allocator<double>>::size_type>(i) + 1]);
+			}
+		}
+		sum = sum * (x[static_cast<std::vector<double, std::allocator<double>>::size_type>(n)] - x[0]) / (3 * n);
+	}
+	else if(g == "d") {
+		do {
+			k++;
+			double h = (b - a) / n;
+			double sum1 = 0;
+			for(int i = 0; i < n; i++) {
+				sum1 += (EvalPolStr(str, a + i * h) + 4 * EvalPolStr(str, a + (i + 0.5) * h) + EvalPolStr(str, a + (i + 1) * h)) * h / 6;
+			}
+			if(abs(sum1 - sum) < eps) {
+				eps1 = abs(sum1 - sum);
+				break;
+			}
+			sum = sum1;
+			n *= 2;
+		} while(true);
+	}
+	out2File(sum, k, eps1);
+}
+void ChebyshevMethod(double& a, double& b, int& n, char*& str) {
+	double sum = 0;
+	double* ti = new double[n];
+
+	//Как в интернете, но c погрешностью
+	//for(int i = 0; i <= n; i++) {
+	//	sum += EvalPolStr(str, (a + b) / 2 + (b - a) / 2 * cos((2 * i + 1) * acos(-1) / (2 * n + 2)));
+	//}
+	//sum *= (b - a) / (n+1);
+
+
+	//Как в методичке, но работает с большой погрешностью
+	std::vector<double> nodes;
+	for(int i = 0; i <= n; ++i) {
+		double xi_prime = (1 + std::pow(-1, i)) / (2.0 * (i + 1));
+		double xi = (a + b) / 2.0 + (b - a) / 2.0 * xi_prime;
+		nodes.push_back(xi);
+	}
+	for(int i = 0; i <= n; i++)
+		sum += EvalPolStr(str, nodes[i]);
+	sum *= (b - a) / n;
+
+
+	std::ofstream out("output.txt");
+	if(!out.is_open()) {
+		return;
+	}
+	out << "I: " << sum << std::endl;
+	out << "ti: ";
+	//for(int i = 0; i < n; i++) {
+	//	ti[i] = (a + b) / 2 + (b - a) / 2 * cos((2 * i + 1) * acos(-1) / (2 * n + 2));
+	//	out << ti[i] << " ";
+	//}
+	for(int i = 0; i <= n; i++) {
+		out << nodes[i] << " ";
+	}
+	out.close();
+	delete[] ti;
+}
+
+int main() {
+	std::ifstream in("input.txt");
+	if(!in.is_open()) {
+		return -1;
+	}
+	int m = 0, n = 0;
+	double a = 0, b = 0, e = 0;
+	std::string g, s;
+	char* str = new char[256];
+	in >> m >> g >> n;
+	std::cout << "m = " << m << "\ng = " << g << "\nn = " << n << std::endl;
+	if(m == 5 || g != "n") {
+		in >> a >> b;
+		std::cout << "a = " << a << " b = " << b << std::endl;
+	}
+	std::vector<double> x(n + 1), y(n + 1);
+	if(g == "n") {
+		for(int i = 0; i <= n; ++i) {
+			in >> x[i];
+			std::cout << "x[" << i << "] = " << x[i] << std::endl;
+		}
+	}
+	in >> s;
+	std::cout << "s = " << s << std::endl;
+	if(g != "d" && m != 5) {
+		if(s == "t") {
+			for(int i = 0; i <= n; ++i) {
+				in >> y[i];
+				std::cout << "y[" << i << "] = " << y[i] << std::endl;
+			}
+		}
+	}
+	if(g == "d" || m == 5) {
+		if(s == "a") {
+			std::string f;
+			in >> f;
+			const char* temp = { f.c_str() };
+			str = CreatePolStr(temp, 0);
+			std::cout << "f(x) = " << f << std::endl;
+		}
+		if(s == "t"){
+			return -1; //несоответствие условию задачи
+		}
+		if(m != 5) {
+			in >> e;
+			std::cout << "E = " << e << std::endl;
+		}
+	}
+	in.close();
+
+	std::ofstream out("output.txt");
+	if(!out.is_open()) {
+		return -1;
+	}
+	switch(m) {
+	case 1:
+		LeftRectangleMethod(a, b, n, g, y, x, str, e);
+		break;
+	case 2:
+		RightRectangleMethod(a, b, n, g, y, x, str, e);
+		break;
+	case 3:
+		TrapezoidMethod(a, b, n, g, y, x, str, e);
+		break;
+	case 4:
+		SimpsonMethod(a, b, n, g, y, x, str, e);
+		break;
+	case 5:
+		ChebyshevMethod(a, b, n, str);
+		break;
+	default:
+		break;
+	}
+
+	return 0;
+}
